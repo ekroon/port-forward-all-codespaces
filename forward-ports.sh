3a4802abca7bfe286c43b7f21e4134ba5159b014
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-09-08 12:15:00 +0200

    Processname is displayName

--- forward-ports.sh
+++ forward-ports.sh
@@ -7,18 +7,18 @@ while overmind status >/dev/null 2>&1; do
 done 
 
 # convert output from command in array by splitting on newline
-IFS=$'\n' read -d '' -r -a codespaces <  <(gh cs list --jq '.[] | select(.state == "Available")' --json state,name | jq -cMr '.name' && printf '\0')
-# read -r -a codespaces <<< "$(gh cs list --jq '.[] | select(.state == "Available")' --json state,name | jq -cMr '.name')"
+IFS=$'\n' read -d '' -r -a codespaces <  <(gh cs list --jq '.[] | select(.state == "Available")' --json state,displayName,name | jq -cMr '.displayName,.name' | sed 's/ /-/g')
 
 # empty file .Procfile
 echo -n "" > .Procfile
 
 # for each item in codespaces add a line to .Procfile
-for codespace in "${codespaces[@]}"; do
-  echo "$codespace: gh cs ssh -c $codespace -- -R 8888:localhost:8888" >> .Procfile
+for ((i=0; i<${#codespaces[@]}; i+=2)); do
+  codespaceName="${codespaces[$i]}"
+  codespace="${codespaces[$i+1]}"
+  echo "$codespaceName: gh cs ssh -c $codespace -- -R 8888:localhost:8888" >> .Procfile
 done
 
 # start overmind to forward ports
-# overmind s -f .Procfile --any-can-die -D -r "$(IFS=, ; echo "${codespaces[*]}")"
 cat .Procfile
 overmind s -f .Procfile --any-can-die -D > /dev/null
